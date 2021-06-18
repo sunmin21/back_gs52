6cb5b2daf6d214c5b87c5c0dd0995fc872d8eb16
wpdbsx <wpdbsx@naver.com>
2021-06-18 19:50:58 +0900

    백

---
+++ GS52/src/main/java/com/gs52/vo/user/UserVO.java
@@ -0,0 +1,22 @@
+package com.gs52.vo.user;
+
+public class UserVO {
+	private String EMP_ID = null;
+	private String EMP_NAME = null;
+	private String EMP_PWD = null;
+	private String EMP_EMAIL = null;
+	private String EMP_ADDRESS = null;
+	private String EMP_PHONE = null;
+	private String EMP_BIRTH = null;
+	private String EMP_INTRO = null;
+	private String EMP_PHOTO = null;
+	private String EMP_ENTRY_DATE = null;
+	private int EMP_RANK_INDEX = 0;
+	private int EMP_DEPT_INDEX = 0;
+	private int EMP_TEAM_INDEX = 0;
+	private int EMP_POSITION_INDEX = 0;
+	private String EMP_BANK_NAME = null;
+	private int EMP_ACCOUNT_NUMBER = 0;
+	private int EMP_VACATION = 0;
+	private String EMP_SALARY = null;
+}
