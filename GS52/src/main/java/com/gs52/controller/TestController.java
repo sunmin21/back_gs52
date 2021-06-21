b0ac163bd49817b57a7eea2114d927b7ec31de24
wpdbsx <wpdbsx@naver.com>
2021-06-21 10:22:19 +0900

    Update TestController.java

--- GS52/src/main/java/com/gs52/controller/TestController.java
+++ GS52/src/main/java/com/gs52/controller/TestController.java
@@ -3,6 +3,7 @@ package com.gs52.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
