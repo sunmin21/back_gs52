72c5a30fb37b6e6ca4673a3ec4705ab5f649b5b5
jiyun0524 <jiyun9705@gmail.com>
2021-07-02 18:24:13 +0900

    0702

--- GS52/src/main/java/com/gs52/vo/schedule/ProjectListVO.java
+++ GS52/src/main/java/com/gs52/vo/schedule/ProjectListVO.java
@@ -1,5 +1,21 @@
 package com.gs52.vo.schedule;
 
 public class ProjectListVO {
-
+	
+	// project 테이블 컬럼들
+	private int PROJECT_INDEX = 0;
+	private String PROJCET_TITLE = null;
+	private String PROJCET_CONTENT = null;
+	private String PROJCET_START = null;
+	private String PROJCET_END = null;
+	private int PROJCET_OKAY = 0;
+	private String PROJCET_DATE = null;
+	
+	// projcet_with 테이블 컬럼들
+	private int PROJECT_WITH_INDEX = 0;
+	private int PROJECT_WITH_EMP_INDEX = 0;
+	private int PROJECT_WITH_OKAY = 0;
+	private String PROJECT_WITH_REJECT = null;
+	private int PROJECT_WITH_LEADER = 0;
+	
 }
