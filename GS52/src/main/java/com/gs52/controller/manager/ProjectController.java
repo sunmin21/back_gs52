4e84197269afa777b5eb0cc25c476c1eb9075482
jiyun0524 <jiyun9705@gmail.com>
2021-07-08 10:11:33 +0900

    0708

---
+++ GS52/src/main/java/com/gs52/controller/manager/ProjectController.java
@@ -0,0 +1,29 @@
+package com.gs52.controller.manager;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gs52.dao.manager.ProjectDAO;
+import com.gs52.vo.schedule.ProjectListVO;
+
+@RestController
+@RequestMapping(value="/manager/project")
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class ProjectController {
+	
+	@Autowired
+	public ProjectDAO plDAO = null;
+	
+	@PostMapping("/selectOkay")
+	public List<ProjectListVO> ProjectListBoard(@RequestBody ProjectListVO vo) {
+		System.out.println("spring => selectOkay");
+		System.out.println(vo);		
+		return plDAO.selectList(vo);
+	}
+}
---
+++ GS52/src/main/java/com/gs52/dao/manager/ProjectDAO.java
@@ -0,0 +1,24 @@
+package com.gs52.dao.manager;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gs52.vo.schedule.ProjectListVO;
+
+@Service
+@Transactional
+public class ProjectDAO {
+	
+	@Autowired
+	private SqlSessionFactory sqlFactory = null;
+	
+	public List<ProjectListVO> selectList(ProjectListVO vo) {
+		System.out.println(vo);
+		return sqlFactory.openSession().selectList("Project.selectOkay",vo);
+	}
+}
