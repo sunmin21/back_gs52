86fe47a964881b93fc50519839bf5f1ce7deb663
jiyun0524 <jiyun9705@gmail.com>
2021-07-08 10:58:06 +0900

    0708

--- GS52/src/main/java/com/gs52/controller/manager/ProjectOkayController.java
+++ GS52/src/main/java/com/gs52/controller/manager/ProjectOkayController.java
@@ -9,16 +9,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gs52.dao.manager.ProjectDAO;
+import com.gs52.dao.manager.ProjectOkayDAO;
 import com.gs52.vo.schedule.ProjectListVO;
 
 @RestController
 @RequestMapping(value="/manager/project")
 @CrossOrigin(origins = "*", maxAge = 3600)
-public class ProjectController {
+public class ProjectOkayController {
 	
 	@Autowired
-	public ProjectDAO plDAO = null;
+	public ProjectOkayDAO plDAO = null;
 	
 	@PostMapping("/selectOkay")
 	public List<ProjectListVO> ProjectListBoard(@RequestBody ProjectListVO vo) {
